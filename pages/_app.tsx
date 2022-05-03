4c8214bbae3ba12fc894274141873a8ea32ff285
Chen Raz <chen_raz@hotmail.com>
2022-05-03 21:48:51 +0300

    Enable react server components with custom layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <span>ADD SOMETHING BEFORE</span>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
